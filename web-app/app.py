6396b18c640c0762cc166642db246c962481e8bf
ezmartini <em4047@nyu.edu>
2022-11-21 16:18:05 -0500

    Added Mongodb connection to both web apps

--- web-app/app.py
+++ web-app/app.py
@@ -4,11 +4,13 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 MONGO = os.getenv('MONGO_REMOTE')
+print(MONGO)
 client = MongoClient(MONGO)
 
-
 app = Flask(__name__)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
