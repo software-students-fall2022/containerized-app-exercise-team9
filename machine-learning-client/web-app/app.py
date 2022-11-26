12f9d99a1398d9949eddd435b44e1e0128f7d67c
Pedro <pbaggio765@gmail.com>
2022-11-26 00:40:41 -0500

    Created methods to generate transcription data

--- machine-learning-client/web-app/app.py
+++ machine-learning-client/web-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
@@ -9,6 +9,19 @@ client = MongoClient(MONGO)
 
 
 app = Flask(__name__)
+
+@app.route("/") 
+def home(): 
+ return render_template("index.html")
+
+@app.route("/record") 
+def record(): 
+ return render_template("record.html")
+
+@app.route("/results") 
+def results(): 
+ return render_template("results.html")
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
