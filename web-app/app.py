042fd20a8c190d43b090936708c67eab4ce0f135
ezmartini <em4047@nyu.edu>
2022-11-26 19:37:04 -0500

    Added a summary table

--- web-app/app.py
+++ web-app/app.py
@@ -20,7 +20,27 @@ def results_home():
 
 @app.route("/all")
 def display_all():
-  return render_template("all.html", documents = db.results.find({}))
+  agg = db.results.aggregate([
+    {
+      "$group": {
+        "_id": "null",
+        "avg_time_taken": { "$avg": "$time_taken"},
+        "avg_accuracy": { "$avg": "$accuracy"},
+        "avg_words_spoken": { "$avg": "$words_spoken"},
+        "avg_correct_spoken": { "$avg": "$correct_words_spoken"},
+        "avg_total_wps": { "$avg": "$total_words_per_second"},
+        "avg_correct_wps": { "$avg": "$correct_words_per_second"},
+      }
+    }
+  ])
+
+  # command cursor to iterable object 
+  # definitely a better way to do this
+  for doc in agg: 
+    data=doc
+
+
+  return render_template("all.html", documents = db.results.find({}), data = data, size = db.results.count_documents({}))
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
